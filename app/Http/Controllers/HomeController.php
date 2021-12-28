6a356c6e3f19371654bd41764d0ca630e4bc84a3
Aboudou Abdel <abd.aa536@gmail.com>
2021-12-28 09:15:02 +0300

    Home Panel Categories Display...

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,9 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Category;
 
 class HomeController extends Controller
 {
+    public static function categoryList(){
+        return Category::where('parent_id', 0)->with('children')->get();
+    }
+
     public function index(){
         return view('layouts.home');
     }
